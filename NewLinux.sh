9ca6e33cc1f95faed66fdbcaaf355530db211778
IanSmith <iansmith@qq.com>
2017-02-06 00:48:33 +0800

    update metasploit download

--- NewLinux.sh
+++ NewLinux.sh
@@ -7,6 +7,7 @@ git clone --depth=1 https://github.com/IanSmith123/vimrc-of-ian.git
 git clone --depth=1 https://github.com/powerline/fonts.git
 wget https://nmap.org/dist/nmap-7.40.tar.bz2
 echo "http://www.rapid7.com/products/metasploit/metasploit-community-thank-you.jsp is the download msf link"
+echo "https://www.rapid7.com/products/metasploit/download/community/thank-you/"
 echo "Your License Key:   8TD3-KQ09-HLP0-EVJR"
 wget https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run
 sudo apt install python-pip -y
