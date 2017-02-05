a3a4715f1b9e1a6169f55da5e42b38be30889b7c
IanSmith <iansmith@qq.com>
2017-02-06 00:42:05 +0800

    update nmap link

--- NewLinux.sh
+++ NewLinux.sh
@@ -5,7 +5,9 @@ git clone --depth=1 https://github.com/sqlmapproject/sqlmap.git
 git clone --depth=1 https://github.com/lijiejie/subDomainsBrute.git
 git clone --depth=1 https://github.com/IanSmith123/vimrc-of-ian.git
 git clone --depth=1 https://github.com/powerline/fonts.git
+wget https://nmap.org/dist/nmap-7.40.tar.bz2
 sudo apt install python-pip -y
+
 sudo apt install figlet -y
 sudo apt install nmap -y
 sudo pip install requests -i https://pypi.douban.com/simpel
