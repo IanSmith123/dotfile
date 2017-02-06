b76a374d8000505d52bf4c0791d5244ec42d99a7
IanSmith <iansmith@qq.com>
2017-02-06 12:17:39 +0800

    update new linux

--- NewLinux.sh
+++ NewLinux.sh
@@ -6,6 +6,7 @@ git clone --depth=1 https://github.com/lijiejie/subDomainsBrute.git
 git clone --depth=1 https://github.com/IanSmith123/vimrc-of-ian.git
 git clone --depth=1 https://github.com/powerline/fonts.git
 wget https://nmap.org/dist/nmap-7.40.tar.bz2
+tar -xvf nmap-7.40
 echo "http://www.rapid7.com/products/metasploit/metasploit-community-thank-you.jsp is the download msf link"
 echo "https://www.rapid7.com/products/metasploit/download/community/thank-you/"
 echo "Your License Key:   8TD3-KQ09-HLP0-EVJR"
@@ -13,8 +14,7 @@ wget https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-
 sudo apt install python-pip -y
 
 sudo apt install figlet -y
-sudo apt install nmap -y
-sudo pip install requests -i https://pypi.douban.com/simpel
-sudo pip install shadowsocks -i https://pypi.douban.com/simpel
-sudo pip install ipython -i https://pypi.douban.com/simpel
+sudo pip install requests -i https://pypi.douban.com/simple
+sudo pip install shadowsocks -i https://pypi.douban.com/simple
+sudo pip install ipython -i https://pypi.douban.com/simple
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"                                                  
