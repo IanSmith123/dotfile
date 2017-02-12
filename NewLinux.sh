15002d778d1c4681c8a009eb664be7261525863a
IanSmith <iansmith@qq.com>
2017-02-12 13:56:21 +0800

    update NewLinux.sh

--- NewLinux.sh
+++ NewLinux.sh
@@ -1,27 +1,47 @@
 #!/bin/bash
+
+
+mkdir ~/forgit
+cd ~/forgit
+git clone --depth=1 https://github.com/IanSmith123/vimrc-of-ian.git
 git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
 git clone --depth=1 https://github.com/rofl0r/proxychains-ng.git
+cd proxychains-ng
+./configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
+sudo make install-config
+echo "socks5 127.0.0.1 1080 " >>/etc/proxychains.conf
+cd ..
+
 git clone --depth=1 https://github.com/sqlmapproject/sqlmap.git
 git clone --depth=1 https://github.com/lijiejie/subDomainsBrute.git
-git clone --depth=1 https://github.com/IanSmith123/vimrc-of-ian.git
 git clone --depth=1 https://github.com/powerline/fonts.git
 
+cd 
 
-wget https://nmap.org/dist/nmap-7.40.tar.bz2
-tar -xvf nmap-7.40.tar.bz2
 
 
 sudo apt install python-pip -y
-sudo apt install figlet -y
-sudo apt install libssl-dev
+sudo apt install figlet sl tree python3-pip -y
+sudo apt install libssl-dev zsh autojump exuberant-ctags sublime-text -y
+
+wget https://nmap.org/dist/nmap-7.40.tar.bz2
+tar -xvf nmap-7.40.tar.bz2
+cd nmap-7.40
+./configure
+make
+sudo make install
+cd ..
 
 
 mkdir ~/.pip
 echo "[global]">~/.pip/pip.conf
 echo "index-url = https://pypi.douban.com/simple/">> ~/.pip/pip.conf
-sudo pip install requests -i https://pypi.douban.com/simple
-sudo pip install shadowsocks -i https://pypi.douban.com/simple
-sudo pip install ipython -i https://pypi.douban.com/simple
+sudo pip install --upgrade pip setuptools
+sudo pip install requests shadowsocks ipython xlrd flask
 
 
 
