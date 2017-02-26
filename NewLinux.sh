5a2a99c9bb86049324bacc71a64c7d6376c5a3bf
IanSmith <iansmith@qq.com>
2017-02-26 11:52:31 +0800

    update before re build kali

--- NewLinux.sh
+++ NewLinux.sh
@@ -4,6 +4,7 @@
 mkdir ~/forgit
 cd ~/forgit
 git clone --depth=1 https://github.com/IanSmith123/vimrc-of-ian.git
+git clone https://github.com/IanSmith123/iansmith123.github.io.git
 git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
@@ -18,7 +19,6 @@ cd ..
 
 git clone --depth=1 https://github.com/sqlmapproject/sqlmap.git
 git clone --depth=1 https://github.com/lijiejie/subDomainsBrute.git
-git clone --depth=1 https://github.com/powerline/fonts.git
 
 cd 
 
@@ -27,6 +27,7 @@ cd
 sudo apt install python-pip -y
 sudo apt install figlet sl tree python3-pip -y
 sudo apt install libssl-dev zsh autojump exuberant-ctags sublime-text -y
+sudo apt install shutter netease-cloud-music okular 
 
 wget https://nmap.org/dist/nmap-7.40.tar.bz2
 tar -xvf nmap-7.40.tar.bz2
@@ -45,10 +46,10 @@ sudo pip install requests shadowsocks ipython xlrd flask
 
 
 
-echo "http://www.rapid7.com/products/metasploit/metasploit-community-thank-you.jsp is the download msf link"
-echo "https://www.rapid7.com/products/metasploit/download/community/thank-you/"
-echo "Your License Key:   8TD3-KQ09-HLP0-EVJR"
-wget https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run
+#echo "http://www.rapid7.com/products/metasploit/metasploit-community-thank-you.jsp is the download msf link"
+#cho "https://www.rapid7.com/products/metasploit/download/community/thank-you/"
+#cho "Your License Key:   8TD3-KQ09-HLP0-EVJR"
+#get https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run
 
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"                                                  
