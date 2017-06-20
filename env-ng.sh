543516d06ac0b2b323b224b0bfd207d377cfabc8
Iansmith's win10 <iansmith@qq.com>
2017-06-20 21:03:12 +0800

    add env-ng, add z, thefuck sudo cp plugin

---
+++ env-ng.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+#######################################################################
+#     File Name           :     env-ng.sh
+#     Created By          :     Les1ie
+#     Email               :     iansmith@qq.com
+#     Creation Date       :     [2017-06-20 20:51]
+#     Last Modified       :     [2017-06-20 21:02]
+#     Description         :     To build a new environment of linux 
+#######################################################################
+echo "########################################################"
+echo "#                                                      #"
+echo "#                  les1ie's tool                       #"
+echo " _                    _   _        "
+echo "| |       ___   ___  / | (_)   ___ "
+echo "| |      / _ \ / __| | | | |  / _ \ "
+echo "| |___  |  __/ \__ \ | | | | |  __/"
+echo "|_____|  \___| |___/ |_| |_|  \___|"
+echo "########################################################"
+
+echo " This is a tool to build a new enviroment for Les1ie, not suite for every one."
+
+
--- zshrc
+++ zshrc
@@ -1,7 +1,8 @@
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 # Path to your oh-my-zsh installation.
-  export ZSH=/home/iansmith/.oh-my-zsh
+#
+  export ZSH=/home/ubuntu/.oh-my-zsh
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
@@ -50,7 +51,7 @@ ZSH_THEME="agnoster"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 #plugins=(git chucknorris)
-plugins=(autojump zsh-256colors tmux git)
+plugins=(zsh-256colors tmux git thefuck z sudo colorize cp)
 source $ZSH/oh-my-zsh.sh
 # User configuration
 # export MANPATH="/usr/local/man:$MANPATH"
