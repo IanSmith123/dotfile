3830dadf4b9de18346e1c9d1760ca1f7c5505d6c
Les1ie <iansmith@qq.com>
2018-08-04 00:48:35 +0800

    add init.sh 初始化脚本

---
+++ init.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+if [$# -eq 0];then
+echo "update,curl,wget,vim,zsh,tmux,docker"
+for cmd in $@
+do
+case $cmd in 
+"update")
+sudo apt update -y
+;;
+
+"curl")
+sudo apt install curl -y
+;;
+
+"wget")
+sudo apt install wget -y
+;;
+
+"vim")
+echo "Vim"
+#sudo apt install vim
+curl -fsSL https://raw.githubusercontent.com/IanSmith123/dotfile/master/vimrc -o ~/.vimrc
+vim -c 'PlugInstall' -c 'qa!'
+;;
+
+"zsh")
+echo "oh-my-zsh"
+sudo apt install zsh figlet -y
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+;;
+
+"tmux")
+echo "tmux"
+sudo apt install tmux -y
+curl -fsSL https://raw.githubusercontent.com/IanSmith123/dotfile/master/tmux.conf -o ~/.tmux.conf
+echo "tmux done"
+;;
+
+"docker")
+curl -fsSL https://get.docker.com -o /tmp/docker.sh
+sudo bash /tmp/docker.sh
+sudo usermod -aG docker $USER
+esac
+
+done
